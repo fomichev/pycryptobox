61cd12f8c0e49db0c44082d123cdc110bbcb1507
Stanislav Fomichev <s@fomichev.me>
2012-05-15 09:37:17 +0400

    revert all changes to form bookmarklet

--- template/bookmarklet/form.js
+++ template/bookmarklet/form.js
@@ -19,21 +19,22 @@ var body = document.getElementsByTagName("body")[0];
 var text = "";
 
 for (var i = 0; i < document.forms.length; i++) {
-	var form_elements =  "len = " + document.forms[i].elements.length;
-	for (var j = 0; j < document.forms[i].elements.length; j++) {
-		var el = document.forms[i].elements[j];
+	var form = document.forms[i];
 
-		if (el.name != "") {
-			if (form_elements == "")
-				form_elements = '\t\t\t"' + el.name + '": "' + el.value + '"';
-			else
-				form_elements += ',\n\t\t\t"' + el.name + '": "' + el.value + '"';
-		} else {
-				form_elements += '((((' + el.id + '))))';
-		}
+	var form_elements =  "";
+	for (var j = 0; j < form.elements.length; j++) {
+		var el = form.elements[j];
+
+		if (el.name == "")
+			continue;
+
+		if (form_elements == "")
+			form_elements = '\t\t\t"' + el.name + '": "' + el.value + '"';
+		else
+			form_elements += ',\n\t\t\t"' + el.name + '": "' + el.value + '"';
 	}
 
-	var form_text = '\t\t"action": "' + document.forms[i].action + '",\n\t\t"method": "' + document.forms[i].method + '",\n\t\t"fields":\n\t\t{\n' + form_elements + '\n\t\t}';
+	var form_text = '\t\t"action": "' + form.action + '",\n\t\t"method": "' + form.method + '",\n\t\t"fields":\n\t\t{\n' + form_elements + '\n\t\t}';
 	text += '{\n\t"type":"site",\n\t"name": "' + name + '",\n\t"address": "' + address + '",\n\t"form":\n\t{\n' + form_text + '\n\t}\n}\n';
 }
 
